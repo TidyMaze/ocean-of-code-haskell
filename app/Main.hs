f2d5a1b1775b24e4ec2b454a7283535db312dbea
yrolland <rolland_yann@orange.fr>
2020-04-17 21:37:09 +0200

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -350,11 +350,6 @@ data Precomputed =
     }
   deriving (Show, Generic)
 
---instance Binary Precomputed
-safeHead :: String -> [a] -> a
-safeHead msg []     = error ("NO HEAD in " ++ msg)
-safeHead msg (x:xs) = x
-
 getMoveAction :: Precomputed -> State -> Maybe Coord -> [Coord] -> (Order, [Coord], Int, Int, Coord)
 getMoveAction precomputed state target myCandidates = (action, newMyCoordHistory, updatedTorpedoCooldown, updatedSonarCooldown, afterCoord)
   where
@@ -371,7 +366,7 @@ getMoveAction precomputed state target myCandidates = (action, newMyCoordHistory
         Just PSonar -> (torpedoCooldown state, max (sonarCooldown state - 1) 0)
         _ -> (torpedoCooldown state, sonarCooldown state)
     afterCoord = maybe curCoord snd maybeMoveWithDest
-    curCoord = safeHead "afterCoord" visited
+    curCoord = head visited
     visited = myCoordHistory state
     visitedSet = S.fromList visited
     maybeMoveWithDest = findMove precomputed visited target
@@ -389,7 +384,7 @@ getMoveActionNoTarget precomputed state = (action, newMyCoordHistory, updatedTor
         Just PTorpedo -> (max (torpedoCooldown state - 1) 0, sonarCooldown state)
         Just PSonar -> (torpedoCooldown state, max (sonarCooldown state - 1) 0)
         _ -> (torpedoCooldown state, sonarCooldown state)
-    afterCoord = maybe (safeHead "afterCoord2" $ myCoordHistory state) snd maybeMoveWithDest
+    afterCoord = maybe (head $ myCoordHistory state) snd maybeMoveWithDest
     maybeMoveWithDest = findMove precomputed (myCoordHistory state) Nothing
 
 explosionDamages :: Coord -> Coord -> Int
@@ -421,11 +416,6 @@ parseSonarResult lastSonarAction sonarResult = lastSonarAction >>= parseNew
 
 buildNewOpponentHistory :: Maybe Order -> [Order] -> [Order]
 buildNewOpponentHistory sonarResultAction orders = maybeToList sonarResultAction ++ orders
-
-getElapsedTime startTime = do
-  endTime <- getCurrentTime
-  let elapsed = diffUTCTime endTime startTime
-  return (show (ceiling (realToFrac (toRational elapsed * 1000))) ++ "ms")
 
 data State =
   State
@@ -550,24 +540,12 @@ findActionsDeprecated precomputed afterParsingInputsState mySetOfShooting oppSet
 
 findCenterOfExplosion :: Precomputed -> [Coord] -> Maybe ([Coord], Int)
 findCenterOfExplosion _ [x] = Just ([x], 2)
-findCenterOfExplosion _ [] = Nothing
-findCenterOfExplosion _ coords | length coords > 9 = Nothing
+findCenterOfExplosion _ coords
+  | length coords > 9 = Nothing
 findCenterOfExplosion precomputed coords = asum [fromCandidates, fromAnyWater]
   where
     fromCandidates = mfilter (not . null . fst) (Just (filter (\c -> all (inExplosionRange c) coords) coords, 1))
     fromAnyWater = mfilter (not . null . fst) (Just (filter (\c -> all (inExplosionRange c) coords) (waterCoords precomputed), 1))
-
-timeoutSample1 =
-  "\"x\\156\\197\\151\\139V\\195 \\f@y\\173\\GS\\175\\253\\255\\US\\204\\239\\240\\203\\212vi\\225\\142X\\173G\\229\\148\\229\\\\\\154&@ 0c\\150\\242j\\156\\233\\158\\166\\132\\229\\215\\ESCc\\229\\241\\USm\\190yD\\207\\179yk\\179\\139\\213\\229\\&7\\244\\158F\\246\\252\\163\\205\\SUB\\244+\\240\\195\\147\\223\\246m\\150\\223\\142\\236\\181\\227P\\236\\217\\US|{\\194\\222\\DEL\\249X\\203K\\183<\\186e0\\148\\ETB\\240\\132v2%\\237\\136\\191I\\225#?\\148\\&3\\244\\132\\ETX\\216\\131\\157\\194W\\232\\147#\\244\\201\\t\\250\\228\\f}r\\129>\\185B_\\216*\\\\\\192\\EM\\156\\192\\DC1,\\227\\&7\\nGp\\STXgp\\SOHW\\240\\r\\254\\133\\GS\\216\\131\\131\\194\\NAK\\\\\\192\\EM,\\253\\191(\\156\\193\\ENQ\\\\\\193\\210\\159I\\225\\n.\\224\\fN\\224\\b\\150\\248\\204\\nGp\\STXgp\\SOHW\\176\\140\\231\\170p\\ENQ\\ETBp\\ACK'p\\EOTk\\237Q\\145\\t\\156\\193\\ENQ\\\\\\193\\&2\\158\\164p\\ENQ\\ETBp\\ACKS\\178\\159\\156\\a-n\\140\\187\\240\\ENQ\\FS\\192Z\\RSK\\n3\\143\\GS\\237\\DC3m\\221r]1\\238\\140\\v\\231-+\\178\\128+\\248\\ACK=r\\ENQS\\210\\SI\\227\\204u\\196u\\206}\\196}\\174\\229\\NAK\\230%a\\237\\FS)\\n\\243\\FS\\209\\242\\\"\\243\\SYN\\243\\DC2\\247=\\247\\&5\\247\\r\\247\\ENQ\\227\\194y\\175\\138\\188\\GSH\\234\\&1\\206\\\\\\a\\220\\199\\220\\231\\204c\\204s\\204\\227\\204\\243<\\135xn\\DEL\\247\\FS\\151\\194s\\147\\231\\234\\217s]xV\\152\\247$\\222\\163x\\207\\226=\\140\\247\\&4\\145\\247\\213\\213V\\221V\\253^\\131\\NAKw\\182{e\\247\\218\\170\\216\\254\\213\\174bU\\NAK\\183:\\r\\162\\241p4\\168Tq\\205k7v\\228\\DEL\\171/\\239\\170VSs\\159;{\\154\\190q\\DEL\\154\\209\\157t\\244'V\\156}\\142\\248\\&27nSs\\136\\149b\\233 V\\178\\242\\218\\200\\175\\220\\&93c\\181v\\145\\182\\150\\186\\254\\236V\\190\\&2\\248\\190H\\131\\246\\175jz\\ETX2\\235\\DC1\\141\""
-
-timeoutSample1Pre =
-  "\"x\\156\\165\\215mN\\132\\&0\\DLE\\NUL\\208\\178P\\160_\\236\\253o\\228\\177\\212\\r\\DC3\\205SV\\141\\252p\\242\\170\\t\\GS\\218\\206\\212\\148\\RS\\207Kz\\254L\\248\\134g\\188\\224\\140w\\\\p\\197\\rw<\\240qF\\231=]\\196\\ESC\\158\\241\\130\\&3\\222q\\193\\NAK7\\220\\241\\192\\145\\143\\223=<\\227\\ENQg\\188\\226\\r\\239\\184\\224\\138\\ESC\\238x\\224\\200g\\190\\136\\v\\206x\\197\\ESC\\222q\\193\\NAK7\\220\\241\\192\\&1\\DEL\\231\\185\\\\\\196\\140W\\188\\225\\138\\ESC\\238x\\224\\152\\159\\223\\205y\\228\\139\\184\\226\\rW\\220p\\199\\ETX\\199\\252\\226=\\DC3v\\159\\186\\238~G\\231m\\220\\240\\142\\v\\174\\184\\225\\142\\a\\142\\252\\226\\189\\tO\\216s\\232\\190v\\159\\184.\\230i\\220q\\193\\NAK7\\220\\241\\192\\145\\175u\\208:\\226\\185\\f/8c\\215\\205\\188\\140\\ENQW\\220p\\199\\ETXG~\\214u\\235\\162u&\\188\\224\\140\\221\\135\\174\\147y\\EM+n\\184\\227\\129#?\\251\\148u\\222\\186i\\157\\178.x\\142\\220g\\174\\139y\\212\\139\\216p\\199\\ETXG~\\222#\\236\\195\\246\\&1\\251\\130u\\216\\186g\\157\\240\\FS\\185\\239\\\\'\\243j\\ETB\\177\\227\\129\\SI\\254.a\\239\\GS\\246m\\251\\160}\\199:o]\\180nx\\206\\220\\151\\174\\163y\\SUB\\a>\\CANO\\216{\\149\\247\\DC2\\251\\188}\\213>f\\221\\183.ZG<w\\238S\\215\\209<\\141\\a1\\225\\171{\\138\\247\\STX\\251\\176}\\203\\186n\\GS\\180nx\\206\\220\\135\\174\\147y\\220?\\197\\233\\237y\\255\\145>\\158\\251\\&9z>\\211\\143\\195\\223=\\DEL\\FS\\158\\252\\US\\226\\233\\240\\227\\253_~\\245l\\248\\223\\DC3\\252\\221\\240+\\CAN\\192\\r\\\\\""
-
-shortEncode :: Binary a => a -> String
-shortEncode e = show $ Zlib.compress $ encode e
-
-shortDecode :: Binary a => String -> a
-shortDecode raw = decode $ Zlib.decompress (read raw :: LBS.ByteString)
 
 findOrders precomputed afterParsingInputsState !myOldCandidates !oppOldCandidates = do
   let !opponentCandidates = findPositionFromHistory precomputed oppOldCandidates (opponentHistory afterParsingInputsState)
@@ -588,7 +566,7 @@ findOrders precomputed afterParsingInputsState !myOldCandidates !oppOldCandidate
   let (!actions, endMyCoordHistory, maybeSonarAction) =
         case attackSeq of
           (x:_) -> trace "rushing" (orders, hist, maybeSonarAction)
-            where (bestSeq, newState) = (\(a, b, c, d) -> (a, b)) . safeHead "bestSeq" $ attackSeq
+            where (bestSeq, newState) = (\(a, b, c, d) -> (a, b)) . head $ attackSeq
                   orders = bestSeq ++ maybeToList (fmap (\(action, _, _, _, _) -> action) maybeMoveFallback)
                   maybeMoveFallback =
                     if any isMoveOrSurface bestSeq
@@ -653,7 +631,6 @@ gameLoop !precomputed !oldState !myOldCandidates !oppOldCandidates = do
           , myLife = myLife
           , oppLife = oppLife
           }
-  debug $ show $ shortEncode afterParsingInputsState
   (!out, !resState, !myNewCandidates, !oppNewCandidates) <- findOrders precomputed afterParsingInputsState myOldCandidates oppOldCandidates
   endTime <- getCurrentTime
   let elapsed = diffUTCTime endTime startTime
@@ -690,25 +667,12 @@ game = do
   endTime <- getCurrentTime
   let elapsed = diffUTCTime endTime startTime
   debug ("spent " ++ show (realToFrac (toRational elapsed * 1000)) ++ " ms")
-  debug $ show $ shortEncode (waterCoords, landMap)
   send $ show startX ++ " " ++ show startY
   let state =
         State
           {myHistory = [], opponentHistory = [], myCoordHistory = [], lastSonarAction = Nothing, torpedoCooldown = 3, sonarCooldown = 4, silenceCooldown = 6, mineCooldown = 3, myLife = 6, oppLife = 6}
   let wc = map (\c -> (c, S.singleton c)) waterCoords
   gameLoop precomputed state wc wc
-
---  debug (show precomputed)
-perf :: IO ()
-perf = do
-  (orders, _, _, _) <- findOrders precomputed state [] []
-  print $ orders
-  print "done"
-  return ()
-  where
-    precomputed = buildPrecomputed waterCoords $ V.fromList $ map V.fromList landMap
-    state = shortDecode timeoutSample1
-    (waterCoords, landMap) = shortDecode timeoutSample1Pre :: ([Coord], [[Bool]])
 
 --  print precomputed
 --  print state
